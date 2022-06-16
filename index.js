22e75b3a4a06ccfe38e040f554137134c462dcee
Anirudh0807 <92206337+Anirudh0807@users.noreply.github.com>
2022-06-16 14:45:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+setInterval(() => {
+    let time = (new Date() - new Date(2003,7,8)) / (31557600000); // milliseconds per year
+    document.getElementById("age").innerHTML = time.toString().substring(0, 12);
+}, 50);
+
+var forEach = function (array, callback, scope) {
+	for (var i = 0; i < array.length; i++) {
+		callback.call(scope, i, array[i]);
+	}
+};
+window.onload = function(){
+	var max = -219.99078369140625;
+	forEach(document.querySelectorAll('.progress'), function (index, value) {
+	percent = value.getAttribute('data-progress');
+		value.querySelector('.fill').setAttribute('style', 'stroke-dashoffset: ' + ((100 - percent) / 100) * max);
+		value.querySelector('.value').innerHTML = percent + '%';
+	});
+}
